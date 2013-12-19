f81b338430f4640c73160986c4a7af72e3d7a356
useyourname <rhuq36@gmail.com>
2013-12-18 21:22:06 -0500

    playing around with intents

--- src/com/hulq/ImgurCam/MyActivity.java
+++ src/com/hulq/ImgurCam/MyActivity.java
@@ -2,6 +2,14 @@ package com.hulq.ImgurCam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.provider.MediaStore;
+import android.content.pm.PackageManager;
+import android.content.Context;
+import android.content.pm.ResolveInfo;
+import java.util.List;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +19,17 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, actionCode);
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
     }
 }
