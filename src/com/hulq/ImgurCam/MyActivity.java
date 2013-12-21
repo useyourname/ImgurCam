468f19e700e76e5a069d8bfe44565b33df493eec
useyourname <rhuq36@gmail.com>
2013-12-20 21:00:36 -0500

    displays full photo in ImageView

--- src/com/hulq/ImgurCam/MyActivity.java
+++ src/com/hulq/ImgurCam/MyActivity.java
@@ -1,21 +1,97 @@
 package com.hulq.ImgurCam;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
+import android.content.Context;
+import java.util.Date;
+import android.os.Environment;
+import java.io.File;
 import android.widget.ImageView;
+import android.content.Intent;
+import java.io.IOException;
+import android.util.Log;
 import android.provider.MediaStore;
 import android.content.pm.PackageManager;
-import android.content.Context;
 import android.content.pm.ResolveInfo;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import android.net.Uri;
+import android.view.View;
+
 
 public class MyActivity extends Activity {
 
     private static final int ACTION_TAKE_PHOTO_B = 1;
     private static final int ACTION_TAKE_PHOTO_S = 2;
     private static final int ACTION_TAKE_VIDEO = 3;
+    private ImageView imageView;
+
+    private String currentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "imgurCam_" + timeStamp;
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+//        File storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try{
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        }catch(IOException ex){
+            System.out.println("createTempFile FAILED");
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                photoFile = null;
+                currentPhotoPath = null;
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, actionCode);
+            }
+        }
+    }//end of dispatchTakePictureIntent(int actionCode)*/
+
+    /*private void dispatchTakePictureIntent(int actionCode) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, actionCode);
+        }
+    }//end of dispatchTakePictureIntent(int actionCode)*/
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Bundle extras = data.getExtras();
+//        Bitmap imageBitmap = (Bitmap) extras.get("data");
+//        imageView.setImageBitmap(imageBitmap);
+        if (resultCode == RESULT_OK) {
+            Log.d("CameraDemo", "Pic saved");
+        }
+        //imgur
+        String clientID = "a0c473fa0852365";
+        String clientSecret = "21e5c9526565e24b842bb4bf108c562e6926d7f7";
+        String url = "http://api.imgur.com/3/image";
+
+        Uri photoUri = Uri.parse(currentPhotoPath);
+        imageView.setImageURI(photoUri);
+    }
 
     /**
      * Called when the activity is first created.
@@ -23,21 +99,11 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
         setContentView(R.layout.main);
-    }
+        dispatchTakePictureIntent(ACTION_TAKE_PHOTO_B);
+        imageView = (ImageView) findViewById(R.id.imageView1);
 
-
-    private void dispatchTakePictureIntent(int actionCode) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(takePictureIntent, actionCode);
-    }
-
-    public static boolean isIntentAvailable(Context context, String action) {
-        final PackageManager packageManager = context.getPackageManager();
-        final Intent intent = new Intent(action);
-        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        return list.size() > 0;
     }
 }
+
+
